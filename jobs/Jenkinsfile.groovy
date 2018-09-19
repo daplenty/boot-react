abab8a027c36bf47cb83cce02132334b06bb65e1
daplenty <daplenty@gmail.com>
2018-09-19 15:46:51 +0200

    Update Jenkinsfile.groovy

--- jobs/Jenkinsfile.groovy
+++ jobs/Jenkinsfile.groovy
@@ -1,3 +1,8 @@
+import org.kohsuke.github.GHRepository
+import org.kohsuke.github.GitHub
+import jenkins.model.Jenkins
+import org.kohsuke.github.GHEvent
+import org.kohsuke.github.GHHook
 
 def Projects = this.class.classLoader.parseClass(readFileFromWorkspace("jobs/registerGitHooks.groovy"))
 
