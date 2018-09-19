482fb1ec42014697593e83dc3e1e9fe9dc3ec368
daplenty <daplenty@gmail.com>
2018-09-19 16:41:21 +0200

    Update registerGitHooks.groovy

--- jobs/registerGitHooks.groovy
+++ jobs/registerGitHooks.groovy
@@ -1,11 +1,7 @@
-import org.kohsuke.github.GHRepository
-import org.kohsuke.github.GitHub
-import jenkins.model.Jenkins
-import org.kohsuke.github.GHEvent
-import org.kohsuke.github.GHHook
 
 class Projects {
 def static calls(String projectName) {
+  List<org.kohsuke.github.GHEvent> events = Arrays.asList(org.kohsuke.github.GHEvent.PUSH, org.kohsuke.github.GHEvent.PULL_REQUEST)
    println projectName 
 }
 }
