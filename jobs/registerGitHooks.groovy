898ceb4def7df82dc8ae931143e7f820f2e98c78
daplenty <daplenty@gmail.com>
2018-09-19 16:55:24 +0200

    Update registerGitHooks.groovy

--- jobs/registerGitHooks.groovy
+++ jobs/registerGitHooks.groovy
@@ -1,8 +1,6 @@
 
 class Projects {
 def static calls(String projectName) {
- withCredentials([string(credentialsId: 'gitpersonaltoken', variable: 'GITHUB_TOKEN')]) {
    println projectName 
- }
 }
 }
