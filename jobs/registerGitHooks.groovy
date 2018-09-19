08407fc0228a3717377fb449d9ee84b31eecd953
daplenty <daplenty@gmail.com>
2018-09-19 15:45:12 +0200

    Update registerGitHooks.groovy

--- jobs/registerGitHooks.groovy
+++ jobs/registerGitHooks.groovy
@@ -1,36 +1,6 @@
-import org.kohsuke.github.GHRepository
-import org.kohsuke.github.GitHub
-import jenkins.model.Jenkins
-import org.kohsuke.github.GHEvent
-import org.kohsuke.github.GHHook
+
 
 def call(String projectName) {
-  withCredentials([string(credentialsId: 'gitpersonaltoken', variable: 'GITHUB_TOKEN')]) {
-    String org = "daplenty"
-    List<GHEvent> events = Arrays.asList(GHEvent.PUSH, GHEvent.PULL_REQUEST)
-    GitHub github = GitHub.connectUsingOAuth(GITHUB_TOKEN)
-    String rootUrl = Jenkins.getActiveInstance().getRootUrl()
-    if (rootUrl == null) {
-      return
-    }
-    GHRepository repo = github.getRepository("${org}/${projectName}")
-    String url = rootUrl + "github-webhook/"
-    try {
-      if (!hookExists(repo, url)) {
-        org.createWebHook(new URL(url), events)
-      }
-    }
-    catch (Throwable e) {
-      echo "Failed to register GitHub Repo Webhook for ${projectName}...Please check why... $e"
-    }
-  }
+ println projectName
 }
 
-boolean hookExists(GHRepository repo, String url) throws IOException {
-  for (GHHook hook : repo.getHooks()) {
-    if (hook.getConfig().get("url").equals(url)) {
-      return true
-    }
-  }
-  return false
-}
